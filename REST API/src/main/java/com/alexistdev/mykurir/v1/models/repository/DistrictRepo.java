a3887c686484f8e9374c7a49cb69c78d2d89a9b6
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-10-12 20:03:04 +0700

    feat(District-Repository): create District Repository unit test

--- REST API/src/main/java/com/alexistdev/mykurir/v1/models/repository/DistrictRepo.java
+++ REST API/src/main/java/com/alexistdev/mykurir/v1/models/repository/DistrictRepo.java
@@ -4,4 +4,5 @@ import com.alexistdev.mykurir.v1.models.entity.District;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DistrictRepo extends JpaRepository<District, Long> {
+    District findByName(String name);
 }
