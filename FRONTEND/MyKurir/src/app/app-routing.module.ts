a470e4304b34c7a0fe7c987ad5b6218ae53a48ec
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2025-05-11 12:02:43 +0700

    chore(MyKurir v1): create dashboard module with dashboard component and sidebar component.

--- FRONTEND/MyKurir/src/app/app-routing.module.ts
+++ FRONTEND/MyKurir/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard/dashboard.component";
+import {LoginComponent} from "./login/login/login.component";
 
 const routes: Routes = [
-  { path:'login', loadChildren:() => import('./login/login.module').then(m=> m.LoginModule)},
-  { path: '', redirectTo: '/login', pathMatch: 'full'}
+
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path:'login', loadChildren:() => import('./login/login.module').then(m=> m.LoginModule) },
+  { path: 'dashboard', loadChildren:() => import('./dashboard/dashboard.module').then(m=>m.DashboardModule) },
 ];
 
 @NgModule({
