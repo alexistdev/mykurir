d2b5e5941fb23aa4486155571e578c21135dc04a
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2025-05-17 20:15:28 +0700

    fix(mobile-menu): fix button expand sidebar on mobile layout

--- FRONTEND/MyKurir/src/app/dashboard/dashboard/dashboard.component.ts
+++ FRONTEND/MyKurir/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  constructor(private renderer: Renderer2) {}
 
+  toggleLargeSidebar() {
+    const htmlElement = document.documentElement; // <html> tag
+    if (htmlElement.classList.contains('sidebar-left-collapsed')) {
+      this.renderer.removeClass(htmlElement, 'sidebar-left-collapsed');
+    } else {
+      this.renderer.addClass(htmlElement, 'sidebar-left-collapsed');
+    }
+  }
 
 }
