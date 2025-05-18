71f3ef2d81e8522845b92ea5f7a921c7fc1997e8
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2025-05-18 17:15:11 +0700

    update project

---
+++ FRONTEND/MyKurir/src/app/dashboard/masteruser/masteruser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-masteruser',
+  templateUrl: './masteruser.component.html',
+  styleUrls: ['./masteruser.component.css']
+})
+export class MasteruserComponent {
+
+
+}
---
+++ FRONTEND/MyKurir/src/app/utils/local-storage.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  setItem(key: string, val:string):void {
+    sessionStorage.setItem(key,val);
+  }
+
+  getItem(key:string):string {
+    return <string>sessionStorage.getItem(key);
+  }
+
+  clearItem():void {
+    sessionStorage.clear();
+  }
+
+  encode(value: string): string {
+    return btoa(
+      encodeURIComponent(value).replace(/%([0-9A-F]{2})/g, (_, p1) =>
+        String.fromCharCode(parseInt(p1, 16))
+      )
+    );
+
+  }
+
+  decode(value: string): string {
+    return decodeURIComponent(
+      Array.prototype.map.call(atob(value), (c: string) =>
+        `%${c.charCodeAt(0).toString(16).padStart(2, '0')}`
+      ).join('')
+    );
+
+  }
+
+
+}
