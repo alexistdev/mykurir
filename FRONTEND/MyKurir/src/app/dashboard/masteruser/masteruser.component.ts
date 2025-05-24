f223712534ca403aae7321d1a268a3767279a195
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2025-05-24 15:25:28 +0700

    fix(pagination): fix pagination by limiting number to 10 and add an elipsis

--- FRONTEND/MyKurir/src/app/dashboard/masteruser/masteruser.component.ts
+++ FRONTEND/MyKurir/src/app/dashboard/masteruser/masteruser.component.ts
@@ -25,7 +25,8 @@ export class MasteruserComponent implements OnInit{
 
   loadData(page: number){
     this.pageNumber = page;
-    this.userService.getUsers(this.pageNumber,10,'id','desc').subscribe({
+    console.log(this.pageNumber);
+    this.userService.getUsers(this.pageNumber,1,'id','desc').subscribe({
       next: (data) => {
         this.payload = data.payload;
         this.pageNumber = this.payload.pageable.pageNumber;
@@ -49,4 +50,30 @@ export class MasteruserComponent implements OnInit{
     }).join(' ');
   }
 
+  getDisplayedPages(): (number | string)[] {
+    const pages: (number | string)[] = [];
+    const total = this.totalPages;
+    const current = this.pageNumber;
+
+    if (total <= 10) {
+      for (let i = 1; i <= total; i++) {
+        pages.push(i);
+      }
+    } else if (current <= 9) {
+      pages.push(1, 2, 3, 4,5,6,7,8,9,10, '...');
+    } else if (current >= total - 2) {
+      pages.push('...',total - 5,total - 4 , total - 3,total - 2, total - 1, total);
+    } else {
+      pages.push('...', current, current + 1, current + 2, current + 3, current + 4, '...');
+    }
+
+    return pages;
+  }
+
+  isNumber(value: any): boolean {
+    return typeof value === 'number';
+  }
+
+
+  protected readonly Number = Number;
 }
